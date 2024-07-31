97f5c7774c452f57cdc17c0b6e176aff5804b4e2
andre10dias <andre10dias@ymail.com>
2024-07-31 16:35:54 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/SistemaGrupoBrasileiroBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupobrasileiro.sistema_grupo_brasileiro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGrupoBrasileiroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGrupoBrasileiroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/SistemaGrupoBrasileiroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupobrasileiro.sistema_grupo_brasileiro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGrupoBrasileiroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
