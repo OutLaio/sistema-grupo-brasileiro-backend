65acc6b3e2b1455aa7ac4a6841e480f275ca9060
andre10dias <andre10dias@ymail.com>
2024-07-31 18:27:16 -0300

    user

---
+++ src/main/resources/db/migration/V1_create_user.sql
@@ -0,0 +1,24 @@
+CREATE SEQUENCE user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE IF NOT EXISTS public.user
+(
+    id bigint NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+    name character varying(255) COLLATE pg_catalog."default",
+    lastname character varying(255) COLLATE pg_catalog."default",
+    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    phonenumber character varying(255) COLLATE pg_catalog."default",
+    password character varying(255) COLLATE pg_catalog."default",
+    sector character varying(255) COLLATE pg_catalog."default",
+    function character varying(255) COLLATE pg_catalog."default",
+    nop character varying(255) COLLATE pg_catalog."default",
+    role character varying(255) COLLATE pg_catalog."default",
+    token text COLLATE pg_catalog."default",
+    CONSTRAINT user_pkey PRIMARY KEY (id),
+    CONSTRAINT uk_471i15k6vbj1lfsfb19getcdi UNIQUE (email),
+    CONSTRAINT uk_4tdehxj7dh8ghfc68kbwbsbll UNIQUE (email)
+)
