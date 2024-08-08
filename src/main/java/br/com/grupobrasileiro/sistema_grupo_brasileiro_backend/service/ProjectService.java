8f5bfd1664bf4999e8c7824f8b75a72d6cec57fb
mikaelleRubia <mikaelle.rubia@outlook.com>
2024-08-08 20:36:39 -0300

    feature: service de create e all para project

--- src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/service/ProjectService.java
+++ src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/service/ProjectService.java
@@ -1,9 +1,12 @@
 package br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.dto.form.ProjectForm;
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.dto.view.ProjectView;
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.mapper.form.ProjectFormMapper;
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.mapper.view.ProjectViewMapper;
@@ -21,6 +24,18 @@ public class ProjectService {
 	
 	@Autowired
 	private ProjectViewMapper projectViewMapper;
+
+
+    @Transactional
+	public ProjectView save(ProjectForm dto){
+		Project entity = projectFormMapper.map(dto);
+		if (!isValidStatus(entity.getStatus())) {
+			throw new IllegalArgumentException("Invalid status value: " + entity.getStatus());
+		}
+        projectRepository.save(entity);
+        return projectViewMapper.map(entity);
+
+	}
 
 	@Transactional(readOnly = true)
 	public ProjectView getProjectById(Long id) {
@@ -49,4 +64,12 @@ public class ProjectService {
 		return status.equals("AF") || status.equals("EA") || status.equals("AA")
 			|| status.equals("AP") || status.equals("EC") || status.equals("CO");
 	}
+	
+	@Transactional
+	public Page<ProjectView> projectAll(PageRequest pageRequest) {
+	    Page<Project> projectPage = projectRepository.findAll(pageRequest);
+	    return projectPage.map(projectViewMapper::map);
+	}
+	
+    
 }
