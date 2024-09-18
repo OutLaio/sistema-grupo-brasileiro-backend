7ebaacfba1f1bf38c316c84ad0ad9234ef2640b6
Renata CHOM <renatachom.ads@gmail.com>
2024-09-18 14:33:46 -0300

    Add new test files to model

---
+++ src/test/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/model/users/UserTest.java
@@ -0,0 +1,14 @@
+package br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.model.users;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
