93feccd1df13d856071cfe9cd9a7e530fc21f221
João Ramos <jvitorsb98@outlook.com>
2024-09-20 10:36:02 -0300

    Teste login

--- src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/controller/auth/AuthController.java
+++ src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/controller/auth/AuthController.java
@@ -14,59 +14,46 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
  * Controlador REST para autenticação e registro de usuários.
- * Este controlador expõe endpoints relacionados à autenticação e operações
- * de registro de colaboradores.
  */
 @RestController
 @RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private EmployeeService employeeService;
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
+    private final UserService userService;
+    private final EmployeeService employeeService;
     private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
 
     /**
      * Endpoint para registrar um novo colaborador.
      *
-     * <p>Este método recebe os detalhes do usuário e do funcionário através do {@link UserDetailsForm},
-     * cria um novo usuário no sistema e, em seguida, persiste os dados do funcionário associados
-     * ao usuário recém-criado.</p>
-     *
-     * <ul>
-     *     <li>Chama o {@code UserService} para validar e criar o usuário com base nas credenciais e dados fornecidos.</li>
-     *     <li>Chama o {@code EmployeeService} para instanciar e persistir o colaborador associado ao usuário.</li>
-     *     <li>Retorna uma resposta com o status HTTP 200 (OK) em caso de sucesso.</li>
-     * </ul>
-     *
      * @param form o {@link UserDetailsForm} contendo os dados de usuário e funcionário.
-     * @return uma resposta HTTP 200 OK em caso de sucesso, ou um erro apropriado se ocorrer alguma validação.
+     * @return uma resposta HTTP 201 Created com os detalhes do colaborador registrado.
      */
     @PostMapping("/register")
-    public ResponseEntity<?> register(@Valid @RequestBody UserDetailsForm form) {
+    public ResponseEntity<EmployeeView> register(@Valid @RequestBody UserDetailsForm form) {
         User user = userService.create(form.userForm());
         EmployeeView employeeView = employeeService.addEmployee(form.employeeForm(), user);
         return ResponseEntity.status(HttpStatus.CREATED).body(employeeView);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginForm form){
+    public ResponseEntity<TokenView> login(@Valid @RequestBody LoginForm form) {
         LOGGER.info("Starting login request for: email={}", form.email());
-        String token = authService.doLogin(form);
+        String token = authService.doLogin(form, authenticationManager);
         LOGGER.info("Successful authentication for: email={}", form.email());
         return ResponseEntity.ok(new TokenView(token));
     }
@@ -80,7 +67,7 @@ public class AuthController {
     }
 
     @PostMapping("/resetPassword")
-    public ResponseEntity<String> resetPassword(@RequestBody @Valid ResetPasswordForm form) {
+    public ResponseEntity<String> resetPassword(@Valid @RequestBody ResetPasswordForm form) {
         LOGGER.info("Starting password reset operation");
         authService.resetPassword(form);
         LOGGER.info("Password successfully reset");
--- src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/service/auth/AuthService.java
+++ src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/service/auth/AuthService.java
@@ -12,41 +12,53 @@ import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.model.users.User;
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.repository.users.UserRepository;
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.service.email.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.internal.util.stereotypes.Lazy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
-public class AuthService {
+public class AuthService implements UserDetailsService {
 
-    private UserRepository userRepository;
-    private AuthenticationManager authenticationManager;
-    private TokenService tokenService;
-    private EmailService emailService;
-    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private  UserRepository userRepository;
 
-    public String doLogin(LoginForm form){
-        User user = (User) userRepository.findByEmail(form.email()).orElseThrow(
-                () -> new EntityNotFoundException("User not found for email: " + form.email())
-        );
+    @Autowired
+    private  TokenService tokenService;
+
+    @Autowired
+    private  EmailService emailService;
+
+    @Autowired
+    private  PasswordEncoder passwordEncoder;
+
+    public String doLogin(LoginForm form, AuthenticationManager authenticationManager) {
+        User user = userRepository.findByEmail(form.email());
+        if (user == null) {
+            throw new EntityNotFoundException("User not found for email: " + form.email());
+        }
         if (user.getDisabled()) throw new UserIsNotActiveException("Acesso negado.");
 
         var usernamePassword = new UsernamePasswordAuthenticationToken(form.email(), form.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
+        var auth = authenticationManager.authenticate(usernamePassword);
 
-        return tokenService.generateToken((User) auth.getPrincipal());
+        // Retornar um token gerado
+        return tokenService.generateToken(user); // Supondo que você tenha um método para gerar o token
     }
 
-    public void requestRecoveryPassword(RecoveryPasswordForm form){
-        User user = (User) userRepository.findByEmail(form.email()).orElseThrow(
-                () -> new EntityNotFoundException("Usuário não encontrado com e-mail: " + form.email())
-        );
+    public void requestRecoveryPassword(RecoveryPasswordForm form) {
+        User user = userRepository.findByEmail(form.email());
+        if (user == null) {
+            throw new EntityNotFoundException("Usuário não encontrado com e-mail: " + form.email());
+        }
 
         String token = tokenService.generateToken(user);
         String resetUrl = "http://localhost:4200/resetPassword?token=" + token;
-
 
         String body = String.format(
                 "Olá!<br><br>" +
@@ -54,30 +66,34 @@ public class AuthService {
                         "<a href=\"%s\">Redefinir Senha</a><br><br>" +
                         "Se você não solicitou isso, ignore este e-mail. Sua senha permanecerá inalterada e nenhuma ação adicional será necessária.<br><br>" +
                         "Atenciosamente,<br>" +
-                        "Grupo Brasileiro",
-                resetUrl
-        );
+                        "Grupo Brasileiro", resetUrl);
 
-        PasswordRequest sendEmailForm = new PasswordRequest(
-                "no-reply@everdev.com",
-                form.email(),
-                "Password Reset",
-                body
-        );
+        PasswordRequest sendEmailForm = new PasswordRequest("no-reply@everdev.com", form.email(), "Password Reset", body);
         emailService.send(sendEmailForm);
     }
 
-    public void resetPassword(ResetPasswordForm form){
+    public void resetPassword(ResetPasswordForm form) {
         String emailFromToken = tokenService.validateToken(form.token());
-        if (emailFromToken == null) throw new InvalidTokenException("Token inválido ou expirado");
+        if (emailFromToken == null) {
+            throw new InvalidTokenException("Token inválido ou expirado");
+        }
 
-        User user = (User)userRepository.findByEmail(emailFromToken).orElseThrow(
-                () -> new EntityNotFoundException("User not found for email: " + emailFromToken)
-        );
+        User user = userRepository.findByEmail(emailFromToken);
+        if (user == null) {
+            throw new EntityNotFoundException("User not found for email: " + emailFromToken);
+        }
 
         user.setPassword(passwordEncoder.encode(form.password()));
         userRepository.save(user);
-
         tokenService.invalidateToken(form.token());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return user; // Certifique-se de que `User` implementa `UserDetails`
     }
 }
