584d7e44d0abca80e667052b334145a06f3472a2
mikaelleRubia <mikaelle.rubia@outlook.com>
2024-10-04 16:31:34 -0300

    fix: em viewMapper e repository

--- src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/mapper/project/view/BriefingViewMapper.java
+++ src/main/java/br/com/grupobrasileiro/sistema_grupo_brasileiro_backend/mapper/project/view/BriefingViewMapper.java
@@ -4,7 +4,13 @@ import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.dto.projects.view
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.mapper.Mapper;
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.mapper.companiesBriefing.view.CompaniesBriefingsViewMapper;
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.mapper.measurement.view.MeasurementsViewMapper;
+import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.model.companies.CompaniesBriefing;
+import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.model.measurements.Measurement;
 import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.model.projects.Briefing;
+import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.repository.companies.CompaniesBriefingRepository;
+import br.com.grupobrasileiro.sistema_grupo_brasileiro_backend.repository.measurements.MeasurementRepository;
+
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,8 +26,16 @@ public class BriefingViewMapper implements Mapper<Briefing, BriefingView> {
     @Autowired
     private CompaniesBriefingsViewMapper companiesBriefingsViewMapper;
 
+    @Autowired
+    private MeasurementRepository measurementRepository;
+
+    @Autowired
+    private CompaniesBriefingRepository companiesBriefingRepository;
+
     @Override
     public BriefingView map(Briefing briefing) {
+        Measurement measurement = measurementRepository.findByBriefingId(briefing.getId());
+        Set<CompaniesBriefing> companiesBriefing= companiesBriefingRepository.findByBriefingId(briefing.getId());
         return new BriefingView(
                 briefing.getId(),
                 briefingTypeMapperView.map(briefing.getBriefingType()),
@@ -29,8 +43,8 @@ public class BriefingViewMapper implements Mapper<Briefing, BriefingView> {
                 briefing.getExpectedTime(),
                 briefing.getFinishTime(),
                 briefing.getDetailedDescription(),
-                measurementsViewMapper.map(briefing.getMeasurement()),
-                companiesBriefingsViewMapper.map(briefing.getCompanies()),
+                measurementsViewMapper.map(measurement),
+                companiesBriefingsViewMapper.map(companiesBriefing),
                 briefing.getOtherCompany()
         );
     }
