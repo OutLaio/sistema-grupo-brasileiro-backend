2c2f812c0ba4dc6c7dc1a5cd00aebc65fe8d5289
Laio Rodrigues <rlaio.ege@gmail.com>
2024-12-10 16:19:01 -0300

    Updating Migrations

--- src/main/resources/db/data/S23__insert_employee_collaborator.sql
+++ src/main/resources/db/data/S23__insert_employee_collaborator.sql
@@ -1,2 +1,2 @@
-INSERT INTO "Tb_Employees" (id_user, name, lastname, phone_number, sector, occupation, agency, avatar)
-VALUES (2, 'Glennie', 'Ondricka', '+123456789', 'Creative', 'Engineer', 'Bashirian, Runte and Spinka', 2);
+INSERT INTO "Tb_Employees" (id_user, name, lastname, phone_number, sector, occupation, agency, registration_number, avatar)
+VALUES (2, 'Glennie', 'Ondricka', '+123456789', 'Creative', 'Engineer', 'Bashirian, Runte and Spinka', '123456', 2);
